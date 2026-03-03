0b47a935a803f30196a0fce697e4c937a91fb49e
Al Amin <alamin.dev01@gmail.com>
2026-03-03 06:18:54 +0600

    added some code

---
+++ recap.js
@@ -0,0 +1,25 @@
+/**
+ * 1. how to use js with html and css
+ * 2. get element or elements from dom 
+ *   2-1 - getElementById
+ *   2-2 - getElementsByClassName
+ *   2-3 - getElementsByTagName
+ *   2-4 - querySelector
+ *   2-5 - querySelectorAll
+ * 3. dynamically add element to the dom : document.createElement
+ *  3.5 : set value to the innerHTML 
+ * 
+ * 4. get and set value from / to a dom element
+ *   4-1 : use innerText (h1, p, span, div)
+ *   4-2 : use value (input, textarea)
+ * 
+ * 5. add event lister (event handler)
+    *   5-1 : click, focus, blur, mouseover, mouseout, keydown, keyup, change
+    *   5-2 : addEventListener onclick = myHandler()
+*  6. use function to reduce duplicate code
+  
+    7. event bubble and event delegate
+ */
+
+document.querySelectorAll('.user-container .disable h3')
+document.querySelectorAll('.my-user li')
---
+++ validation.js
@@ -0,0 +1,16 @@
+const number = 23
+// staring , number , boolean
+if (typeof number === 'number') {
+    console.log('Value is a number')
+}
+else {
+    console.log('Value is not a number')
+}
+
+const numbers = [12, 32, 12, 55];
+const student = {
+    name: 'Rongila Rafsan',
+    job: 'khaoya daoya '
+}
+console.log(typeof student)
+console.log(Array.isArray(numbers))
